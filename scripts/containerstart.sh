63d9be0cb1d21977cd359219a290c321c3887cf3
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2023-12-03 13:30:48 +0530

    Update containerstart.sh

--- scripts/containerstart.sh
+++ scripts/containerstart.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull vishwask22/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 vishwask22/simple-python-flask-app
